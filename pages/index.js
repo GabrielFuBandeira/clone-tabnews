b21869cc94bc8458eeb77365662bdd6aaa30f19e
Gabriel <gfb.gabriel@hotmail.com>
2025-01-04 16:55:40 -0300

    prettier corrigiu arquivo index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>I´m Gabriel and I Love Patrícia e Elo, my wife and daughter!</h1>
+  return <h1>I´m Gabriel and I Love Patrícia e Elo, my wife and daughter!</h1>;
 }
 
 export default Home;
