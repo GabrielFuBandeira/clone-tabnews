ad33524730368ec41e0ff4764c71887bf0dfd011
Gabriel Fuck Bandeira <36495467+GabrielFuBandeira@users.noreply.github.com>
2025-01-04 19:36:44 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>I´m Gabriel and I Love Patrícia e Elo, my wife and daughter!</h1>
+  return <h1>I´m Gabriel and I Love Patrícia e Elo, my wife and daughter!</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
