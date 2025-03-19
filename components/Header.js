70dc6de7c5b316b01cd71a59bf8737a359d89536
Gabriel <gfb.gabriel@hotmail.com>
2025-03-19 15:35:23 -0300

    Testando alterações no layout

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header style={{ background: "#222", color: "white", padding: "10px" }}>
+      <h1>Meu Site</h1>
+    </header>
+  );
+}
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
+import Layout from "../components/Layout";
+
 function Home() {
-  return <h1>I´m Clark!</h1>;
-}
-
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("indentação errada");
+  return (
+    <Layout>
+      <h1>I'm Clark!</h1>
+    </Layout>
+  );
 }
 
 export default Home;
